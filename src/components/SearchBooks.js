cc1871ee5660c6d45306ca8fa89628f5666b1a5a
Mohsin Karim <mohsinkar@users.noreply.github.com>
2020-04-01 10:47:43 -0500

    Updated - SearchBooks
     SearchBooks components to use Hooks

--- src/components/SearchBooks.js
+++ src/components/SearchBooks.js
@@ -1,68 +1,47 @@
-import React, { Component } from 'react'
+import React, { useState } from 'react'
 import { Link } from 'react-router-dom'
 import BookShelfBooks from './BookShelfBooks'
 import { search } from '../BooksAPI'
 import { update } from '../BooksAPI'
 
 
-export default class SearchBooks extends Component {
+function SearchBooks(props) {
 
-    state = {
-        query: "",
-        books: [],
-        libraryBooks: []
-    }
+    const [query, setQuery] = useState("");
+    const [books, setBooks] = useState([]);
+    const [libraryBooks] = useState([...props.location.state.libraryBooks.currentlyReading, ...props.location.state.libraryBooks.wantToRead, ...props.location.state.libraryBooks.read]);
 
-    componentDidMount() {
-     
-        const { currentlyReading, wantToRead, read } =this.props.location.state.libraryBooks
-        this.setState({
-            libraryBooks: [...currentlyReading, ...wantToRead, ...read]
-        })
-    }
-
-    changeHandler = (query) => {
-        this.setState({
-            query
-        })
+    const changeHandler = (query) => {
+        setQuery(query)
         if (query.length > 0) {
             search(query).then(res => {
                 let bookShelf = res;
-                bookShelf = bookShelf.map(obj => this.state.libraryBooks.find(o => o.id === obj.id) || obj);
-                this.setState({
-                    books: bookShelf
-                });
+                if (bookShelf.length > 0) {
+                    bookShelf = bookShelf.map(obj => libraryBooks.find(o => o.id === obj.id) || obj);
+                    setBooks(bookShelf)
+                }
             })
         } else {
-            this.setState({
-                books: []
-            });
+            setBooks([])
         }
     }
 
-    handleLibrarySelection = (book, shelf) => {
-        update(book, shelf).then(res => {
-            this.props.history.push('/')
-        })
-    }
+    const handleLibrarySelection = (book, shelf) => update(book, shelf).then(res => props.history.push('/'))
 
-
-    render() {
-
-        return (
-            <div>
-                <div className="search-books">
-                    <div className="search-books-bar">
-                        <Link to="/"><button className="close-search">Close</button></Link>
-                        <div className="search-books-input-wrapper">
-                            <input type="text" placeholder="Search by title or author" value={this.state.query} onChange={(event) => (this.changeHandler(event.target.value))} />
-                        </div>
-                    </div>
-                    <div className="search-books-results">
-                        <BookShelfBooks books={this.state.books} title="Search Results" selectionChanges={this.handleLibrarySelection} />
+    return (
+        <div>
+            <div className="search-books">
+                <div className="search-books-bar">
+                    <Link to="/"><button className="close-search">Close</button></Link>
+                    <div className="search-books-input-wrapper">
+                        <input type="text" placeholder="Search by title or author" value={query} onChange={(event) => (changeHandler(event.target.value))} />
                     </div>
                 </div>
+                <div className="search-books-results">
+                    <BookShelfBooks books={books} title="Search Results" selectionChanges={handleLibrarySelection} />
+                </div>
             </div>
-        )
-    }
+        </div>
+    )
 }
+export default SearchBooks
